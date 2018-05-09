3fe5b289f86114d01565f3ad245fdc7061885629
Brian Kendzior <bkendzior@gmail.com>
2018-05-09 13:22:03 -0700

    Update build_webpage.sh

--- scripts/build_webpage.sh
+++ scripts/build_webpage.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 tar -xvf ~/scm_artifact.tar -C ~/
-
 ls -l ~/scm_artifact
 
+echo "----------------------------"
 echo "Building Webpage"
+echo "----------------------------"
 
+# Run CURL Commands to get details about build
 touch ~/scm_artifact/webpage.html
 
-tar -cvf ~/build_artifact.tar ~/scm_artifact
+echo "----------------------------"
+echo "Webpage Built"
+echo "----------------------------"
 
+tar -cvf ~/build_artifact.tar ~/scm_artifact
 ls -l ~/
