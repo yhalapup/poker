b0601c3a211d52abe95d1309cc0be413dd2a016b
Brian Kendzior <bkendzior@gmail.com>
2018-05-09 13:21:04 -0700

    Update run_unit_tests.sh

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 tar -xvf ~/scm_artifact.tar -C ~/
-
 ls -l ~/
 
+echo "----------------------------"
 echo "Running Unit Tests"
+echo "----------------------------"
 
+# Do Unit Tests Here
 sleep 30
 
+echo "----------------------------"
 echo "Unit Tests Passed"
+echo "----------------------------"
