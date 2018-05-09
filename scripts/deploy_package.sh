1390592e5545f683b29a647a3e213fedbba00d78
Brian Kendzior <bkendzior@gmail.com>
2018-05-09 13:46:02 -0700

    Update deploy_package.sh

--- scripts/deploy_package.sh
+++ scripts/deploy_package.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-tar -xvf ~/build_artifact.tar -C ~/
-ls -la ~/
+cd ~
+
+tar -xvf build_artifact.tar
+ls -la
 
 echo "----------------------------"
 echo "Deploying Package"
 echo "----------------------------"
 
 sleep 30 # Work Goes Here
+cat scm_artifact/webpage.html
 
 echo "----------------------------"
 echo "Success"
