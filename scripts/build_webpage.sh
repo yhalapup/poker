218b9eacc2ce83350a7dcfb4b4b2125dd5c34f12
Brian Kendzior <bkendzior@gmail.com>
2018-05-09 13:44:54 -0700

    Update build_webpage.sh

--- scripts/build_webpage.sh
+++ scripts/build_webpage.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-tar -xvf ~/scm_artifact.tar -C ~/
-ls -l ~/
+cd ~
+
+tar -xvf scm_artifact.tar
+ls -l
 
 echo "----------------------------"
 echo "Building Webpage"
 echo "----------------------------"
 
 # Run CURL Commands to get details about build
-echo "Hello World" > ~/scm_artifact/webpage.html
+echo "Hello World" > scm_artifact/webpage.html
 
 echo "----------------------------"
 echo "Webpage Built"
 echo "----------------------------"
 
-tar -cvf ~/build_artifact.tar ~/scm_artifact
-ls -l ~/
+tar -cvf build_artifact.tar scm_artifact
+ls -l
