2b44669cb2474c70d058ac0c3c2b0672565655c5
Brian Kendzior <bkendzior@gmail.com>
2018-05-09 12:24:27 -0700

    Update fetch_and_tar_repo.sh

--- scripts/fetch_and_tar_repo.sh
+++ scripts/fetch_and_tar_repo.sh
@@ -5,5 +5,6 @@ mkdir scm_artifact
 touch ./scm_artifact/file1 ./scm_artifact/file2 ./scm_artifact/file3 ./scm_artifact/file4
 
 tar -cvf scm_artifact.tar scm_artifact
+chmod +x scm_artifact.tar
 
 ls -la
