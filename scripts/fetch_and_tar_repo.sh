df6879a46031dcb0508e7867a08e0ac9723e1715
Brian Kendzior <bkendzior@gmail.com>
2018-05-09 13:23:42 -0700

    Update fetch_and_tar_repo.sh

--- scripts/fetch_and_tar_repo.sh
+++ scripts/fetch_and_tar_repo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "----------------------------"
-echo "Fetching Repository
+echo "Fetching Repository"
 echo "----------------------------"
 
 # Todo: pull code here
@@ -9,7 +9,7 @@ mkdir ~/scm_artifact
 touch ~/scm_artifact/file1 ~/scm_artifact/file2 ~/scm_artifact/file3 ~/scm_artifact/file4
 
 echo "----------------------------"
-echo "Repository Fetched
+echo "Repository Fetched"
 echo "----------------------------"
 
 tar -cvf ~/scm_artifact.tar ~/scm_artifact
