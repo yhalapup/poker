bb05e344429a60d65814499b268f3bd8fed7cb32
Brian <bkendzior@gmail.com>
2018-05-09 17:29:44 -0700

    Replace

--- scripts/build_canary_webpage.sh
+++ scripts/build_canary_webpage.sh
@@ -17,12 +17,8 @@ DAY=`date $DATESTRING -u +%d`
 TIME=`date $DATESTRING -u +"%k:%M:%S UTC"`
 SECONDS=`date $DATESTRING -u +%s`
 
-sed "s/year not set/$YEAR/g" scm_artifact/index.html
-sed "s/month not set/$MONTH/g" scm_artifact/index.html
-sed "s/day not set/$DAY/g" scm_artifact/index.html
-sed "s/time not set/$TIME/g" scm_artifact/index.html
-sed "s/seconds not set/$SECONDS/g" scm_artifact/index.html
-
-cat scm_artifact/index.html
-
-exit 1
+sed -i "s/year not set/$YEAR/g" scm_artifact/index.html
+sed -i "s/month not set/$MONTH/g" scm_artifact/index.html
+sed -i "s/day not set/$DAY/g" scm_artifact/index.html
+sed -i "s/time not set/$TIME/g" scm_artifact/index.html
+sed -i "s/seconds not set/$SECONDS/g" scm_artifact/index.html
